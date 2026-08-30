6f2bc089c248836c21a9dc7f9135205986f1448f
Manaskosht <koshtimanas@gmail.com>
2026-08-30 11:53:53 +0530

    update

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app=FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173/"],
+    CORSMiddleware,allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 @app.get("/api/health")
 def health():
-    return {"Message": "Hello from FastAPI backend ✅"}
+    return {"Message":"Hello from fastapi Backend ✅"}
